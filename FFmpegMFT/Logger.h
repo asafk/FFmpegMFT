1c7dae0bfaa9cbd848739768794b4c166338ecec
Kave, Asaf <kaveasaf@gmail.com>
2019-02-04 11:11:52 +0200

    Change logger to use args in the methods

--- FFmpegMFT/Logger.h
+++ FFmpegMFT/Logger.h
@@ -1,15 +1,34 @@
 #pragma once
 #include <string>
+#include <log4cpp/Category.hh>
+#include <log4cpp/PropertyConfigurator.hh>
 
 using namespace std;
 
 class Logger
 {
 private:
-	Logger();
+	Logger()
+	{
+		std::string initFileName = "FFmpegMFT.dll.properties";
+
+		try
+		{
+			log4cpp::PropertyConfigurator::configure(initFileName);
+			LogInfo("********* start logging... *********");
+		}
+		catch (log4cpp::ConfigureFailure& logExc)
+		{
+			//Log config not found, not using logging facilities
+		}
+	}
 
 public:	
-	~Logger();
+	~Logger()
+	{
+		LogInfo("********* end logging... *********");
+		log4cpp::Category::shutdown();
+	}
 
 	static Logger& getInstance()
 	{
@@ -20,9 +39,48 @@ public:
 		return instance;
 	}
 
-	void LogDebug(string strDebug);
-	void LogInfo(string strInfo);
-	void LogWarn(string strWarn);
-	void LogError(string strError);
+	template<typename... Args> 
+	void LogDebug(const char* stringFormat, Args... args)
+	{
+		log4cpp::Category& GeneralLogger = 
+		log4cpp::Category::getInstance(std::string("GeneralLogger"));
+		if(GeneralLogger.isDebugEnabled())
+		{
+			GeneralLogger.debug(stringFormat,args...);
+		}
+	}
+
+	template<typename... Args> 
+	void LogInfo(const char* stringFormat, Args... args)
+	{
+		log4cpp::Category& GeneralLogger = 
+		log4cpp::Category::getInstance(std::string("GeneralLogger"));
+		if(GeneralLogger.isInfoEnabled())
+		{
+			GeneralLogger.info(stringFormat,args...);					
+		}
+	}
+
+	template<typename... Args> 
+	void LogWarn(const char* stringFormat, Args... args)
+	{
+		log4cpp::Category& GeneralLogger = 
+		log4cpp::Category::getInstance(std::string("GeneralLogger"));
+		if(GeneralLogger.isWarnEnabled())
+		{
+			GeneralLogger.warn(stringFormat,args...);				
+		}
+	}
+
+	template<typename... Args> 
+	void LogError(const char* stringFormat, Args... args)
+	{
+		log4cpp::Category& GeneralLogger = 
+		log4cpp::Category::getInstance(std::string("GeneralLogger"));
+		if(GeneralLogger.isErrorEnabled())
+		{
+			GeneralLogger.error(stringFormat,args...);				
+		}
+	}
 };
 
